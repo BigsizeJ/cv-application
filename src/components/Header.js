01eddcfbaba427206fab8a8b0c7c6fe5a866fea9
BigsizeJ <apacjessie@gmail.com>
2022-08-07 21:07:06 +0800

    Finish preview of uploaded image

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Create Resume</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Resume.js
@@ -0,0 +1,13 @@
+import Left from "./Left";
+import Right from "./Right";
+
+const Resume = () => {
+  return (
+    <div className="resume">
+      <Left />
+      <Right />
+    </div>
+  );
+};
+
+export default Resume;

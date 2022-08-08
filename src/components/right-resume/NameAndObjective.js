2dd1eefe1c259eb0436b1497f33e7fb5873e5b07
BigsizeJ <apacjessie@gmail.com>
2022-08-08 21:17:56 +0800

    finished the fullname, objective

--- src/components/right-resume/NameAndObjective.js
+++ src/components/right-resume/NameAndObjective.js
@@ -1,19 +1,69 @@
 import React, { useState, useEffect } from "react";
 
 const NameAndObjective = () => {
-  const [fullname, setFullname] = useState("");
-  const [position, setPosition] = useState("");
-  const [objective, setObjective] = useState("");
+  const [fullname, setFullname] = useState({ value: "", show: false });
+  const [position, setPosition] = useState({ value: "", show: false });
+  const [objective, setObjective] = useState({ value: "", show: false });
 
-  useEffect(() => {}, [fullname, position, objective]);
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (!e.target.classList.contains("input")) {
+        removeFocus();
+      }
+    };
+    document.addEventListener("click", handleClick);
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, [fullname, position, objective]);
+
+  const handleClick = (e) => {
+    removeFocus();
+    if (e.target.classList.contains("fullname")) {
+      setFullname({ ...fullname, show: true });
+    }
+    if (e.target.classList.contains("position")) {
+      setPosition({ ...position, show: true });
+    }
+  };
+
+  const removeFocus = () => {
+    setFullname({ ...fullname, show: false });
+    setPosition({ ...position, show: false });
+    setObjective({ ...objective, show: false });
+  };
 
   return (
     <div className="name-objective">
       <div className="name">
-        <h1>Jessie Apac</h1>
-        <p className="position">Software Engineer</p>
+        {fullname.show ? (
+          <input
+            type="text"
+            className="fullname-input input"
+            onChange={(e) => {
+              setFullname({ ...fullname, value: e.target.value });
+            }}
+          ></input>
+        ) : (
+          <h1 className="input fullname" onClick={handleClick}>
+            {fullname.value.length <= 0 ? "Fullname" : fullname.value}
+          </h1>
+        )}
+        {position.show ? (
+          <input
+            type="text"
+            className="position-input input"
+            onChange={(e) => {
+              setPosition({ ...position, value: e.target.value });
+            }}
+          ></input>
+        ) : (
+          <p className="input position" onClick={handleClick}>
+            {position.value.length <= 0 ? "Job position" : position.value}
+          </p>
+        )}
       </div>
-      <p className="objective">
+      <p className="input objective">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
         nisl augue, hendrerit a consequat ac, sagittis vel ligula. Cras felis
         nisi, pulvinar eu accumsan id, ornare hendrerit enim. Suspendisse leo
